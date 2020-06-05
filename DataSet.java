b67b85f170cef25e5e6b8c9776eaafe754160d0c
mberlins <62808050+mberlins@users.noreply.github.com>
2020-06-06 00:02:43 +0200

    Add files via upload

--- DataSet.java
+++ DataSet.java
@@ -15,24 +15,12 @@ public class DataSet
     {
         Incident tmp = new Incident();
 
+
         System.out.print("\n\nPrinting initialized \n\n\n\n");
         for (int i=0; i < incidents.size(); i++)
         {
-            tmp.setAge(incidents.get(i).getAge());
-            tmp.setSex(incidents.get(i).getSex());
-            tmp.setCp(incidents.get(i).getCp());
-            tmp.setTrestbps(incidents.get(i).getTrestbps());
-            tmp.setChol(incidents.get(i).getChol());
-            tmp.setFbs(incidents.get(i).getFbs());
-            tmp.setRestecg(incidents.get(i).getRestecg());
-            tmp.setThalach(incidents.get(i).getThalach());
-            tmp.setExang(incidents.get(i).getExang());
-            tmp.setOldpeak(incidents.get(i).getOldpeak());
-            tmp.setSlope(incidents.get(i).getSlope());
-            tmp.setCa(incidents.get(i).getCa());
-            tmp.setThal(incidents.get(i).getThal());
-            tmp.setNum(incidents.get(i).getNum());
 
+            tmp = incidents.get(i);
             tmp.print();
             System.out.print("\n");
 
